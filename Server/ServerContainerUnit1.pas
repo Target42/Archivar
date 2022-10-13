907553f1cdf9142e34123d4eae169029ebe471b9
Target42 <stephan.winter@online.de>
2022-10-13 16:50:06 +0200

    add midas-lib

--- Server/ServerContainerUnit1.pas
+++ Server/ServerContainerUnit1.pas
@@ -13,7 +13,7 @@ uses
   System.SyncObjs, System.Generics.Collections, FireDAC.Stan.Intf,
   FireDAC.Stan.Option, FireDAC.Stan.Param, FireDAC.Stan.Error, FireDAC.DatS,
   FireDAC.Phys.Intf, FireDAC.DApt.Intf, FireDAC.Stan.Async, FireDAC.DApt,
-  FireDAC.Comp.DataSet, FireDAC.Comp.Client;
+  FireDAC.Comp.DataSet, FireDAC.Comp.Client, MidasLib;
 
 type
   TServerContainer1 = class(TService)
