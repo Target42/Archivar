172e3acaf47f14edc085ffb88dd45b370d2879ad
Target42 <stephan.winter@online.de>
2023-02-07 21:14:39 +0100

    some changes

---
+++ Setup/u_texte.pas
@@ -0,0 +1,18 @@
+unit u_texte;
+
+interface
+
+const
+  SearchGDSText  =
+  'Suchen der FireBird-Installation'#10;
+  InfoGDSText    =
+  'Der Archivserver sucht die Firebird-Installation.'#10+
+  'Wenn auch ein Backup der Datenbank erfolgen soll, so mus auf die Tools'+
+  'von FireBird zugegriffen werden können.'#10+
+  'Ansonsten reicht die fbclient.dll für die Kommunikation mit dem Server.'#10+
+  'Zu Problemen kann es weiterhin führen, wenn im suchpfad eine weiter fbclient.dll gefunden wird.'#10#10;
+
+
+implementation
+
+end.
