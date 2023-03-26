884190c78be165dbf97ba0d592d9dd94e9cf4249
Target42 <stephan.winter@online.de>
2023-03-26 21:00:13 +0200

    add create lin on desktop

--- Launcher/f_main.pas
+++ Launcher/f_main.pas
@@ -78,9 +78,55 @@ var
 implementation
 
 uses
-  IdHashMessageDigest, u_json, System.IOUtils, system.zip, f_proxy;
+  IdHashMessageDigest, u_json, System.IOUtils, system.zip, f_proxy,
+  ShlObj, ComObj, ActiveX;
 
 {$R *.dfm}
+
+function GetDesktopFolder: string;
+var
+  PIDList: PItemIDList;
+  Buffer: array [0..MAX_PATH-1] of Char;
+begin
+  Result := '';
+  SHGetSpecialFolderLocation(Application.Handle, CSIDL_DESKTOP, PIDList);
+  if Assigned(PIDList) then
+    if SHGetPathFromIDList(PIDList, Buffer) then
+      Result := Buffer;
+end;
+
+function CreateDesktopShellLink(const TargetName: string): Boolean;
+var
+  IObject: IUnknown;
+  ISLink: IShellLink;
+  IPFile: IPersistFile;
+  PIDL: PItemIDList;
+  LinkName: string;
+  InFolder: array [0..MAX_PATH-1] of Char;
+begin
+  Result := False;
+
+  IObject := CreateComObject(CLSID_ShellLink);
+  ISLink := IObject as IShellLink;
+  IPFile := IObject as IPersistFile;
+
+  with ISLink do
+  begin
+    SetDescription('Description ...');
+    SetPath(PChar(TargetName));
+    SetWorkingDirectory(PChar(ExtractFilePath(TargetName)));
+  end;
+
+  SHGetSpecialFolderLocation(0, CSIDL_DESKTOPDIRECTORY, PIDL);
+  SHGetPathFromIDList(PIDL, InFolder) ;
+
+  LinkName := IncludeTrailingBackslash(GetDesktopFolder);
+  LinkName := LinkName + ExtractFileName(TargetName) + '.lnk';
+
+  if not FileExists(LinkName) then
+    if IPFile.Save(PWideChar(LinkName), False) = S_OK then
+      Result := True;
+end;
 
 procedure TMainForm.BitBtn1Click(Sender: TObject);
 var
@@ -171,6 +217,7 @@ begin
     StatusBar1.SimpleText := 'Abgeschlossen';
     Screen.Cursor := crDefault;
 
+    CreateDesktopShellLink( TPath.Combine(m_root, 'Archivar.exe' ) );
     if flag then
       startProgram;;
   except
