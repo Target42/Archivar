679439375e15223b3e83bd6a7139aca0e25446ea
Stephan Winter <wahren.testpin@online.de>
2024-11-04 20:20:56 +0100

    Bugfix for packing

--- Pack.bat
+++ Pack.bat
@@ -1,13 +1,15 @@
 set HOME=d:\temp\ArchivarServer
+
+del %GITHOME%\Archivar\bin\server\www_dnl\server.zip
+
 xcopy /S %GITHOME%\Archivar\bin\Server\*.* %HOME%\
+
 rd /S /Q %HOME%\log
-rm %HOME%\.gitignore
-rm %HOME%\ArchivServer.exe.ini
-attrib +H %HOME%\FDConnectionDefs.ini
-attrib +H %HOME%\libeay32.dll
-attrib +H %HOME%\libzmq-win32.dll
-attrib +H %HOME%\ssleay32.dll
+del /S %HOME%\.gitignore
+del %HOME%\ArchivServer.exe.ini
+
 powershell Compress-Archive -LiteralPath 'd:\temp\ArchivarServer' -DestinationPath "d:\temp\Server.zip"
 copy d:\temp\Server.zip %GITHOME%\Archivar\bin\server\www_dnl
+
 del d:\temp\server.zip
 rd /S /Q d:\temp\ArchivarServer
